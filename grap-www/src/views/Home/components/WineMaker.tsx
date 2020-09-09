ad9db97ce06d1a4e3090e888240e090491e1795c
Madao <madao.chen@flexport.com>
2020-09-10 00:17:20 +0800

    new homepage

---
+++ grap-www/src/views/Home/components/WineMaker.tsx
@@ -0,0 +1,116 @@
+import React, {useMemo} from "react";
+import styled from "styled-components";
+import CountUp from "react-countup";
+import numeral from "numeral";
+
+import CardContent from "../../../components/CardContent";
+
+import {getDisplayBalance} from "../../../utils/formatBalance";
+import BigNumber from "bignumber.js";
+import Button from "../../../components/Button";
+
+interface WineMakerProps {
+  wineNumber?: number;
+  wineMakingScore?: number;
+  unclaimedNumber?: number;
+}
+const WineMaker: React.FC<WineMakerProps> = ({
+  wineMakingScore,
+  wineNumber,
+  unclaimedNumber,
+}) => {
+  const claimedWine = () => {};
+  const draw = () => {};
+  return (
+    <StyledStats>
+      <Card>
+        <CardContent>
+          <StyledStat>
+            <StyledValue>
+              <CountUp end={wineMakingScore} />
+            </StyledValue>
+            <Label text="Your WineMaking Score" />
+          </StyledStat>
+        </CardContent>
+      </Card>
+
+      <StyledSpacer />
+
+      <Card>
+        <CardContent>
+          <StyledStat>
+            <StyledValue>
+              <CountUp end={wineNumber} />
+            </StyledValue>
+            <Label text="Owned Wines" />
+          </StyledStat>
+        </CardContent>
+      </Card>
+
+      <StyledSpacer />
+
+      <Card>
+        <CardContent>
+          <StyledStat>
+            <StyledValue>
+              {unclaimedNumber > 0 ? (
+                <Button
+                  onClick={claimedWine}
+                  text={` ${unclaimedNumber} New Crypto Wines earned!`}
+                />
+              ) : (
+                ""
+              )}
+            </StyledValue>
+          </StyledStat>
+        </CardContent>
+      </Card>
+    </StyledStats>
+  );
+};
+
+const StyledStats = styled.div`
+  width: 325px;
+`;
+
+const StyledStat = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledValue = styled.span`
+  font-size: 36px;
+  font-weight: 700;
+  margin-bottom: 10px;
+`;
+
+const StyledSpacer = styled.div`
+  height: ${(props) => props.theme.spacing[4]}px;
+`;
+
+const Card: React.FC = ({children}) => <StyledCard>{children}</StyledCard>;
+
+const StyledCard = styled.div`
+  background: #ffb3b3;
+  color: #000;
+  border: 1px solid ${(props) => props.theme.color.grey[300]}ff;
+  border-radius: 12px;
+  box-shadow: inset 1px 1px 0px ${(props) => props.theme.color.grey[100]};
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  overflow: hidden;
+`;
+
+interface LabelProps {
+  text?: string;
+}
+
+const Label: React.FC<LabelProps> = ({text}) => (
+  <StyledLabel>{text}</StyledLabel>
+);
+
+const StyledLabel = styled.div`
+  color: #999;
+`;
+export default WineMaker;
---
+++ grap-www/src/views/Home/components/WinePool.tsx
@@ -0,0 +1,119 @@
+import React, {useCallback, useEffect, useState} from "react";
+import styled from "styled-components";
+import Countdown, {CountdownRenderProps} from "react-countdown";
+
+import Button from "../../../components/Button";
+import CardContent from "../../../components/CardContent";
+import Dial from "../../../components/Dial";
+import Label from "../../../components/Label";
+import {NavLink} from "react-router-dom";
+import useGrap from "../../../hooks/useGrap";
+
+interface WinePoolProps {
+  ticketNumber: number;
+}
+
+const WinePool: React.FC<WinePoolProps> = ({ticketNumber}) => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  let dateNumber = Date.parse(`${year}-${month}-${day} 00:00 GMT+0000`);
+  if (Date.now() > dateNumber) {
+    dateNumber += 24 * 60 * 60;
+  }
+  const dialValue = ((dateNumber - Date.now()) / (1000 * 60 * 60 * 24)) * 100;
+  const renderer = (countdownProps: CountdownRenderProps) => {
+    const {hours, minutes, seconds} = countdownProps;
+    const paddedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+    const paddedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    const paddedHours = hours < 10 ? `0${hours}` : hours;
+    return (
+      <span>
+        {paddedHours}:{paddedMinutes}:{paddedSeconds}
+      </span>
+    );
+  };
+
+  const draw = () => {};
+
+  return (
+    <StyledWinePool>
+      <Card>
+        <CardContent>
+          <Dial size={232} value={dialValue ? dialValue : 0}>
+            <StyledCountdown>
+              <StyledCountdownText>
+                <Countdown date={new Date(dateNumber)} renderer={renderer} />
+              </StyledCountdownText>
+              <Label text="Next Airdrop" />
+            </StyledCountdown>
+          </Dial>
+          <StyledSpacer />
+          <Button
+            onClick={draw}
+            text={`Draw(Cost 100)`}
+            disabled={ticketNumber < 100}
+          />
+          <StyledLink to="/wines/:s">Earn Tickets</StyledLink>
+        </CardContent>
+      </Card>
+    </StyledWinePool>
+  );
+};
+
+const StyledWinePool = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 330px;
+  box-sizing: border-box;
+  min-height: 420px;
+`;
+
+const StyledSpacer = styled.div`
+  height: ${(props) => props.theme.spacing[4]}px;
+`;
+const Card: React.FC = ({children}) => <StyledCard>{children}</StyledCard>;
+
+const StyledCard = styled.div`
+  background: #ffb3b3;
+  color: #000;
+  border: 1px solid ${(props) => props.theme.color.grey[300]}ff;
+  border-radius: 12px;
+  box-shadow: inset 1px 1px 0px ${(props) => props.theme.color.grey[100]};
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  overflow: hidden;
+`;
+const StyledCountdown = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+`;
+const StyledCountdownText = styled.span`
+  color: ${(props) => props.theme.color.grey[600]};
+  font-size: 36px;
+  font-weight: 700;
+`;
+
+const StyledLink = styled(NavLink)`
+  box-sizing: border-box;
+  color: ${(props) => props.theme.color.red[100]};
+  font-size: 24px;
+  font-weight: 700;
+  padding: ${(props) => props.theme.spacing[3]}px
+    ${(props) => props.theme.spacing[4]}px;
+  text-align: center;
+  text-decoration: none;
+  width: 100%;
+  &:hover {
+    color: ${(props) => props.theme.color.red[500]};
+  }
+  &.active {
+    color: ${(props) => props.theme.color.primary.main};
+  }
+`;
+
+export default WinePool;
